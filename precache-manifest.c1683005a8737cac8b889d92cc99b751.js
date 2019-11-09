1c2009cdbec8679dab1e6f378a18c47adb3e4b5e
= <=>
2019-11-09 16:53:54 +0530

    Updates

---
+++ precache-manifest.c1683005a8737cac8b889d92cc99b751.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db3a29a4d468223dbdc9d57945611f1c",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "f0894b081768f39708fa",
+    "url": "/monster-rolodex/static/css/main.6d4bfa04.chunk.css"
+  },
+  {
+    "revision": "b0b58fe607faa4d03a43",
+    "url": "/monster-rolodex/static/js/2.de1aa811.chunk.js"
+  },
+  {
+    "revision": "f0894b081768f39708fa",
+    "url": "/monster-rolodex/static/js/main.0a6de5e2.chunk.js"
+  },
+  {
+    "revision": "6f70f97b8cfdb2d06998",
+    "url": "/monster-rolodex/static/js/runtime-main.aec4d7d7.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/monster-rolodex/static/media/logo.25bf045c.svg"
+  }
+]);
